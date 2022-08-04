6d175077ae3aac5da8fb37e59bb7a239fa576800
Ale i <allleizq@gmail.com>
2022-08-03 21:32:34 -0700

    added routes in correct place

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,8 +1,1 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./postRoutes');
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-
-module.exports = router;
