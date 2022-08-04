100cbddb67b43e1dc6b4dc67185ae70319823eac
Ale i <allleizq@gmail.com>
2022-08-03 18:59:59 -0700

    added date helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,3 +2,9 @@
 // this is where i will be doing the formate time/date thing
 //reference activity 16-sessions for help if needed
 
+module.exports = {
+    format_date: (date) => {
+      // Format date as MM/DD/YYYY
+      return date.toLocaleDateString();
+    },
+}
