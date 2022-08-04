9f7b5e6fc61800ccf58f135b39d9508aa7c28a09
Ale i <allleizq@gmail.com>
2022-08-03 23:21:24 -0700

    fix seed error in order to be able to actually populate data and created post route

--- models/index.js
+++ models/index.js
@@ -4,35 +4,42 @@ const Post = require('./Post');
 const Comment = require ('./Comment')
 
 
- //ale can have many posts 
+//  ale can have many posts 
 User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
-  });
+});
   
 
 Post.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
-  });
+});
 
   //post hasMany comments
   //Ale can make many comments
 Post.hasMany(Comment, {
     foreignKey: "post_id",
     onDelete: "CASCADE",
-  });
+});
 
- //Ale can have many comments
+Comment.hasOne(Post, {
+  foreignKey: "post_id",
+});
+
+//  Ale can have many comments
 User.hasMany(Comment, {
-    foreignKey: "username",
+    foreignKey: "user_id",
     onDelete: "CASCADE",
-  });
+});
 
 //comments 
 Comment.belongsTo(Post, {
     foreignKey: "post_id",
-  });
+});
 
+Comment.belongsTo(User, {
+    foreignKey: "post_id", 
+});
   
   module.exports = { User, Post, Comment };
