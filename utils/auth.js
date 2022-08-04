87e44b253f4a301a56c2d6d87f3a2269452313e2
Ale i <allleizq@gmail.com>
2022-08-03 20:49:32 -0700

    added seed data

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,5 @@
 const withAuth = (req, res, next) => {
-    // If the user is not logged in, redirect the request to the login route
+    // authorizes user
     if (!req.session.logged_in) {
       res.redirect('/login');
     } else {
