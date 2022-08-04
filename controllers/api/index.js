fedbee6a34e7724cfcd0efdd59714fb322fd716c
Ale i <allleizq@gmail.com>
2022-08-03 21:42:54 -0700

    updated routes inside index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,11 @@
 // this index is going to be responsible for our model routes???
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const postsRoutes = require('./postRoutes');
+const commentsRoutes = require('./commentsRoute');
+
+router.use('/users', userRoutes);
+router.use('/posts', postsRoutes);
+router.use('/comments', commentsRoutes);
+
+module.exports = router;
