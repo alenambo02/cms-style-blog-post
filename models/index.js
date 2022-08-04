ba8347a7e2c704bbd68fadc46365cc51cd816b78
Ale i <allleizq@gmail.com>
2022-08-03 18:58:55 -0700

    added comment and index model

--- models/index.js
+++ models/index.js
@@ -1,1 +1,38 @@
 //relationship 
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require ('./Comment')
+
+
+ //ale can have many posts 
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+  //post hasMany comments
+  //Ale can make many comments
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE",
+  });
+
+ //Ale can have many comments
+User.hasMany(Comment, {
+    foreignKey: "username",
+    onDelete: "CASCADE",
+  });
+
+//comments 
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+  });
+
+  
+  module.exports = { User, Post, Comment };
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,4 @@
 //comments for self
 // this is where i will be doing the formate time/date thing
 //reference activity 16-sessions for help if needed
+
