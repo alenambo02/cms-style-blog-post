8a847724af35cc597d882d4b938cb0650cf300b0
Ale i <allleizq@gmail.com>
2022-08-04 16:59:17 -0700

    added routes for post

--- controllers/api/postsRoutes.js
+++ controllers/api/postsRoutes.js
@@ -2,69 +2,69 @@ const router = require('express').Router();
 const { Post, User} = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
+
   try {
-    // Get all posts and JOIN with user data
     const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+      include: [{ model: User }],
     });
+   
+
     res.status(200).json(postData);
-
-    
-
-    
-    
-    // Serialize data so the template can read it, get rides of the extra material
-    // const posts = postData.map((post) => post.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    // res.render('homepage', { 
-    //   posts, 
-    //   logged_in: req.session.logged_in 
-    // });
+    console.log()
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/post/:id', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
     });
 
-    const post = postData.get({ plain: true });
-
-    res.render('post', {
-      ...post,
-      logged_in: req.session.logged_in
-    });
+    res.status(200).json(newPost);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
-// Use withAuth middleware to prevent access to route
+router.put('/dashboard/:id', withAuth, async (req, res) => {
+    
+  try {
+    const postData = await Post.update(
+      { content: req.body.content }, 
+      {
+        where: {id: req.params.id}
+      }
+    );
 
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/post');
-    return;
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
   }
 
-  res.render('post');
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -5,7 +5,7 @@ const withAuth = require('../utils/auth');
 //do i need comment? cause its not like ima comment my own post ??
 
 // get that users route to all their post 
-router.get('/', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
 
   try {
     const postData = await Post.findAll({
@@ -23,6 +23,18 @@ router.get('/', withAuth, async (req, res) => {
   } catch (err) {
     res.redirect('login');
   }
+});
+
+
+router.post('/dashboard', withAuth, async (req, res) => {
+    
+  try {
+    const postData = await Post.create(req.body);
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
 });
   
   
